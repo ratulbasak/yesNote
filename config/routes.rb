8c265a753d08d28decd741b5575b7cce0d6f7609
ratul <ratulbasak93@gmail.com>
2016-11-10 19:51:23 +0600

    ruby on rails

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  devise_for :users
+  get 'welcome/index'
+
+  root 'welcome#index'
+  
+  resources :notes
+
+  authenticated :user do
+	  root "notes#index", as: "authenticated_root"
+	end
+end
